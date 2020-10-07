3fd79952c93e04ef28f6dd1b2853fbb1b22743b3
Newland-A <ajn252@gmail.com>
2020-10-07 00:12:19 -0400

    create owners table

---
+++ animal_information_vets/db/migrate/20201007040139_create_owners_table.rb
@@ -0,0 +1,14 @@
+class CreateOwnersTable < ActiveRecord::Migration
+  def change
+    create_table :owners do |t|
+      t.integer :owner_id
+      t.string :username
+      t.string :password_digest
+      t.string :name
+      t.string :location
+      t.integer :pet_id
+      t.datetime :create_at
+      t.datetime :updated_at
+    end
+  end
+end
