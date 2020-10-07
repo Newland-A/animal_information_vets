c7f732182fd1097a94be075c4dca0b3fa486446a
Newland-A <ajn252@gmail.com>
2020-10-07 00:00:19 -0400

    add attributes to users table

--- animal_information_vets/db/migrate/20201007033816_create_users_table.rb
+++ animal_information_vets/db/migrate/20201007033816_create_users_table.rb
@@ -1,7 +1,11 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.timestamp :create_at
-      t.timestamp :updated_at
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.datetime :create_at
+      t.datetime :updated_at
+    end
   end
 end
