38cd0dfdabfb8a36351823ec9cd416f29549595a
Newland-A <ajn252@gmail.com>
2020-10-08 14:50:12 -0400

    add post method for pets

--- animal_information_vets/app/controllers/users_controller.rb
+++ animal_information_vets/app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
+  
+  get '/signup' do
+    erb :'users/signup'
+  end
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to 'signup'
+    else
+      # binding.pry
+      if params[:vet_owner] == "is_vet"
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password], :is_vet => true)
+      else
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password], :is_owner => true) 
+      end
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/pets/new'
+    end
+  end
 end
