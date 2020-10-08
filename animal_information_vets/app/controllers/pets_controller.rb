5d04356275652eef276fc1105611a7799a03ed0f
Newland-A <ajn252@gmail.com>
2020-10-08 14:54:09 -0400

    add get and post method for pets and new

--- animal_information_vets/app/controllers/pets_controller.rb
+++ animal_information_vets/app/controllers/pets_controller.rb
@@ -1,3 +1,21 @@
 class PetsController < ApplicationController
 
+  get '/pets' do
+    @pets = Pet.all
+    erb :'pets/index'
+  end
+
+  get '/pets/new' do
+    @user = User.all
+    erb :"pets/new"
+  end
+  
+  post '/pets/new' do
+    @pets = current_user.pets.build(params)
+    if @pet.save
+      redirect '/pets/#{@pets.id}'
+    else
+      redirect '/pets/new'
+    end
+  end
 end
