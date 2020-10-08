7c15853290415236a59ed9aafc5db602201f9f91
Newland-A <ajn252@gmail.com>
2020-10-08 14:50:59 -0400

    add post login and get logout method

--- animal_information_vets/app/controllers/sessions_controller.rb
+++ animal_information_vets/app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
 
+  get '/login' do
+    erb :'sessions/login'
+  end
+
+  post '/login' do
+    #find the user
+    user = User.find_by(email: params[:email])
+    #authenticate the user somehow
+    if user && user.authenticate(params[:password])
+        # log the user in and redirect
+        session[:user_id] = user.id
+        redirect "/pets"
+    else
+        redirect "/login"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect "/login"
+  end
 end
